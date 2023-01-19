a53ba0b6d1788f354a22e5edbc394c62b3e68602
MartinMl <Mml@gmail.com>
2023-01-19 11:46:46 -0600

    moreno-components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import styled from "styled-components";
+const Button = styled.button`
+background: ${(props) => (props.primary ? "palevioletred" : "white")};
+  color: ${(props) => (props.primary ? "white" : "green")};
+
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+`;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Martin Moreno Lopez</h1>
+      <h2>Practica components</h2>
+      <Button as="a" href="https://utd.edu.mx">UTD</Button>
+      <Button primary as="a" href="https://github.com/martinYulo15">GIT-HUB</Button>
+      <Button as="a" href="https://www.instagram.com/martin_ml15/">Redes sociales</Button>
     </div>
   );
 }
